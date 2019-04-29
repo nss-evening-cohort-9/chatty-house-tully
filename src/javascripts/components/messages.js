d2f0186e408dc9ab3286c745fe835112650e55f4
Calvin J Foster <cal.j.foster@gmail.com>
2019-04-29 18:38:09 -0500

    fixed cards so they will display the correct features

--- src/javascripts/components/messages.js
+++ src/javascripts/components/messages.js
@@ -10,10 +10,8 @@ const domStringBuilder = (messageArray) => {
     domString += '<div class="card" style="width: 18rem;">';
     domString += '<div class="card-body">';
     domString += `<h5 class="card-title">${message.user}</h5>`;
-    domString += '<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>';
     domString += `<p class="card-text">${message.message}</p>`;
-    domString += `<a href="#" class="card-link">${message.time}</a>`;
-    domString += '<a href="#" class="card-link">Another link</a>';
+    domString += `<class="card-link">${message.time}</a>`;
     domString += '</div>';
     domString += '</div>';
   });
