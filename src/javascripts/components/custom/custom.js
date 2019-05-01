87b59b360d0ac0331f9fe8a731cd1d5705bda314
Andrew Tiller <tiller.andrew@gmail.com>
2019-04-30 21:29:55 -0500

    wip, primary and secondary colors are working. cancel and save not behaving quite correctly

--- src/javascripts/components/custom/custom.js
+++ src/javascripts/components/custom/custom.js
@@ -1,18 +1,31 @@
 import $ from 'jquery';
 
-let colorWell;
+let primarySelector;
+let secondarySelector;
+let primary;
+let secondary;
 
 function updateAll(event) {
-  document.querySelectorAll('p').forEach((p) => {
-    const paragraphs = p;
-    paragraphs.style.color = event.target.value;
-  });
+  if (event.target.id === 'primary') {
+    primary = event.target.value;
+  } else if (event.target.id === 'secondary') {
+    secondary = event.target.value;
+  }
 }
 
+const submit = () => {
+  $('#customSheet').remove();
+  $(`<style type="text/css" id="customSheet">.custom { background-color: ${primary} } .custom .card { background-color: ${secondary} }</style>`).appendTo('head');
+};
+
 function startup() {
-  colorWell = $('#primary');
-  colorWell.change(updateAll);
-  colorWell.select();
+  primarySelector = $('#primary');
+  primarySelector.change(updateAll);
+  primarySelector.select();
+  secondarySelector = $('#secondary');
+  secondarySelector.change(updateAll);
+  secondarySelector.select();
+  $('#submit').click(submit);
 }
 
 export default { startup };
--- src/javascripts/components/themes/themes.js
+++ src/javascripts/components/themes/themes.js
@@ -1,9 +1,17 @@
 import $ from 'jquery';
 import './themes.scss';
 
-// const lightify = () => {
 
-// };
+const customify = () => {
+  if ($('body').hasClass('dark')) {
+    $('body').removeClass('dark');
+  }
+  if ($('body').hasClass('light')) {
+    $('body').removeClass('light');
+  }
+
+  $('body').addClass('custom');
+};
 
 const darkify = () => {
   if ($('body').hasClass('light')) {
@@ -29,11 +37,17 @@ const lightify = () => {
 
 
 const listeners = () => {
+  $('#cancel').click(() => {
+    lightify();
+  });
   $('#light').click(() => {
     lightify();
   });
   $('#dark').click(() => {
     darkify();
+  });
+  $('#custom').click(() => {
+    customify();
   });
 };
 
